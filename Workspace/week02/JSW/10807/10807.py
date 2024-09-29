053a075d3cc9d2fdf0c5f128028c573e0ca6b9fb
qda-sw <jsw0510@pusan.ac.kr>
2024-09-29 15:22:18 +0900

    update jsw/week2

--- Workspace/week02/JSW/10807/10807.py
+++
@@ -1,7 +0,0 @@
-N = int(input())
-nums = list(map(int, input().split()))
-v = int(input())
-
-result = [e for e in nums if e == v]
-# result = filter(lambda x: x == v, nums)
-print(len(list(result)))
--- Workspace/week02/JSW/1330/1330.py
+++
@@ -1,3 +0,0 @@
-A, B = map(int, input().split())
-result_message = ">" if A > B else "==" if A == B else "<"
-print(result_message)
