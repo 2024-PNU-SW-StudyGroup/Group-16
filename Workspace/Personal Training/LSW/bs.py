b57e186285382a4da7d0ba0834a4423d11389df3
ChopinBag <sw6129@pusan.ac.kr>
2024-11-07 17:26:32 +0900

    Add tasks.json and launch.json for Clang configuration

--- Workspace/Personal Training/LSW/bs.py
+++ Workspace/Personal Training/LSW/bs.py
@@ -1,5 +1,8 @@
+import os
+print(os.getcwd())  # 현재 작업 디렉토리 출력
 from bs4 import BeautifulSoup
 
+"""
 with open('MyTest.html', 'r', encoding='utf-8') as file:
     html_content = file.read()
 
@@ -9,3 +12,4 @@ green_texts = soup.find_all(class_='red')
 
 for text in green_texts:
     print(text.get_text())
+"""
